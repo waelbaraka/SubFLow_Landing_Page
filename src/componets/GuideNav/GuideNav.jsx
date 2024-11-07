981346ac953e98d66def1e907062b59231a45b9d
waelbaraka <pytsud@gmail.com>
2024-11-07 17:00:08 +0400

    sdfds

--- src/componets/GuideNav/GuideNav.jsx
+++ src/componets/GuideNav/GuideNav.jsx
@@ -11,12 +11,12 @@
           card3: 'improve customer satisfaction, reduce response times, and enhance overall operational efficiency.freeing up agents to focus on more complex issues.',
         },
         predictiveAnalytics: {
-          card1: 'Our Al-powered sentiment analysis digs deep into your customers online conversations,Our Al-powered sentiment analysis digs deep into your customers online conversations, revealing their true emotions and opinions.Know what they love, what they hate, and why they tick.',
+          card1: 'Our AI-powered sentiment analysis dives deep into your customers online conversations, capturing nuanced insights into their true emotions and opinions. This technology goes beyond simple positive or negative feedback, identifying subtle sentiments, tones, and contexts that reveal what customers love, what frustrates them, and what motivates their loyalty. By analyzing social media posts, reviews, and customer feedback in real-time.',
           card2: 'Understand customer satisfaction, identify brand sentiment, and track public opinion in real-time.',
           card3: 'Reclaim time to focus on innovation, customer relationships, and business development.',
         },
         leadQualification: {
-          card1: 'Our AI assistants Identifies Hot Leads Quickly pinpoints ready-to-buy prospects,Our AI assistants Identifies Hot Leads Quickly pinpoints ready-to-buy prospectsby analyzing vast amounts of data. This allows you to prioritize your efforts and close deals faster',
+          card1: 'Our AI assistants swiftly identify high-quality leads, analyzing vast amounts of data to pinpoint ready-to-buy prospects with precision. By examining behavioral patterns, engagement history, and demographic insights, the AI assistant distinguishes potential buyers from casual browsers. This process enables your sales team to focus their efforts on the most promising leads, reducing time spent on unqualified prospects',
           card2: 'Delegate outreach messages like emails and social media posts.Assign lead prioritization and report generation to virtual sales assistants.',
           card3: 'Aess each rep’s sales qualification skills with virtual coaches.Improve competitive intelligence.',
         },
